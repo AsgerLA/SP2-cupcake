2e891d4b8d52bc07be556852f582a91164db7800
Linus-llm <linusmolgaard@gmail.com>
2025-10-20 16:52:24 +0200

    Topping, Bottom, Order and User entities created. Attributes, constructor and get/setters added to each entity.

---
+++ src/main/java/entities/Order.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Order {
+    private int id;
+    private String topping;
+    private String bottom;
+    private double price;
+
+    public Order(int id, String topping, String bottom, double price) {
+        this.id = id;
+        this.topping = topping;
+        this.bottom = bottom;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTopping() {
+        return topping;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+
+    public String getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(String bottom) {
+        this.bottom = bottom;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class User {
+    private int id;
+    private String email;
+    private double balance;
+    private boolean admin;
+
+    public User(int id, String email, double balance, boolean admin) {
+        this.id = id;
+        this.email = email;
+        this.balance = balance;
+        this.admin = admin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+}
