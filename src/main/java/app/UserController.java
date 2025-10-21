2e607044595a3f9ee6e52a652afd9721471bd92b
AsgerLA <cph-aa645@stud.ek.dk>
2025-10-21 09:40:12 +0200

    database connection pool

--- src/main/java/app/UserController.java
+++ src/main/java/app/UserController.java
@@ -3,6 +3,10 @@ package app;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class UserController {
     public static void addRoutes(Javalin app)
     {
@@ -11,6 +15,26 @@ public class UserController {
 
     public static void serveIndexPage(Context ctx)
     {
+        // FIXME: for debugging
+        Connection conn = null;
+        PreparedStatement ps;
+        ResultSet rs;
+        try {
+            conn = Server.db.connect();
+            String sql = "SELECT Toppings.name FROM Toppings";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+            return;
+        } finally {
+            if (conn != null)
+                Server.db.close(conn);
+        }
+
         ctx.render(Path.Template.INDEX);
     }
 }
